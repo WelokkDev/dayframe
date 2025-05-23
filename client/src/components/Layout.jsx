27a49798c2c043444462f1045becc5b1c9fcb110
Daniel Vargas <daniel.vargas@live.ca>
2025-05-22 21:48:22 -0400

    Set up Express server and PostgreSQL connection

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
   return (
     <div className="flex h-[100dvh] w-[100dvw] p-8 pb-20 gap-16 sm:p-20 bg-background font-sans antialiased">
       <Navbar />
-      <div className="bg-foreground h-[90dvh] w-full rounded-4xl p-4">
+      <div className="bg-foreground h-[100%] w-full rounded-4xl p-4">
         {children}
       </div>
     </div>
---
+++ server/index.js
@@ -0,0 +1,26 @@
+// server/index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const pool = require('./db'); // Import shared db connection
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is working!");
+});
+
+// test db connection
+app.get('/db-test', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ time: result.rows[0].now });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: 'Database connection failed' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
